0dba008a7508d349b342f4686b638fe7fc051ded
Andrés Ragot <andresragot99@gmail.com>
2024-10-02 16:46:45 +0200

    Update Vaquita.cpp

--- Vaquita.cpp
+++ Vaquita.cpp
@@ -4,14 +4,17 @@
 
 Vaquita::Vaquita(std::string nombre, int _leche) : raza{ nombre }, leche{ _leche } {}
 
-std::string Vaquita::get_raza() {
+std::string Vaquita::get_raza() 
+{
 	return raza;
 }
 
-int Vaquita::get_leche() {
+int Vaquita::get_leche() 
+{
 	return leche;
 }
 
-void Vaquita::Imprimir() {
+void Vaquita::Imprimir() 
+{
 	std::cout << "Vaquita " << get_raza() << ": " << get_leche() << " litros." << std::endl;
 }
